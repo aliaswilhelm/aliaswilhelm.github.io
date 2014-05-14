66e0af68519eac21187cf9e140e01080502ceb66
aliaswilhelm <aliawilhelm2014@u.northwestern.edu>
2014-05-14 17:47:40 +0200

    summary commit
    initial commit

---
+++ script.js
@@ -0,0 +1,59 @@
+$(document).ready(function(){
+	
+	slideshows = $('div.slideshow');
+	
+	$('div.titles p').click(setSlideshow);
+	$('.sb').click(setSlideshow)
+	$(document).keydown(function(e){
+		switch (e.which) {
+			case 37:
+				$("div.content .slideshow").slickPrev();
+				break;
+			case 39:
+				$("div.content .slideshow").slickNext();
+				break;
+			default:
+				return;
+		}
+	});
+
+});
+
+var setSlideshow = function() {
+	story = $(this).attr("class");
+	console.log(story);
+
+		$("img.bigphoto").hide();
+
+		for (var i = 0; i <= slideshows.length - 1; i++) {
+			s = $(slideshows[i]);
+
+			if (s.attr("id") == story) {
+				$("div.content").append(slideshows[i]);
+				s.show();
+			} else {
+				s.hide();
+			}
+
+		};
+
+		$("div.content .slideshow").slick({
+		});
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$('css selector').event
